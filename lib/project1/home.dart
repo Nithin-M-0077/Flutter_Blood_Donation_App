1142536b92726fb54a562202e08702d86f91caf1
Nithin-M-007 <isnithinm2001@gmail.com>
2023-12-08 11:45:53 +0530

    some fixes

--- lib/project1/home.dart
+++ lib/project1/home.dart
@@ -10,7 +10,7 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   final CollectionReference donor =
-      FirebaseFirestore.instance.collection('donor');
+  FirebaseFirestore.instance.collection('donor');
 
   void deleteDonor(docId) {
     donor.doc(docId).delete();
@@ -35,101 +35,118 @@ class _HomePageState extends State<HomePage> {
           Navigator.pushNamed(context, '/add');
         },
         backgroundColor: Colors.red,
-        child: Icon(Icons.add, size: 45,
+        child: Icon(
+          Icons.add,
+          size: 45,
         ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
-
       body: Padding(
         padding: const EdgeInsets.all(10.0),
         child: StreamBuilder(
           stream: donor.snapshots(),
           builder: (context, AsyncSnapshot snapshot) {
-            if (snapshot.hasData) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(
+                child: CircularProgressIndicator(color: Colors.red,),
+              );
+            } else if (snapshot.hasError) {
+              return Center(
+                child: Text('Error: ${snapshot.error}'),
+              );
+            } else if (snapshot.hasData) {
               return ListView.builder(
-                  itemCount: snapshot.data!.docs.length,
-                  itemBuilder: (context, index) {
-                    final DocumentSnapshot donorSnap = snapshot.data.docs[index];
-                    return Padding(
-                      padding: const EdgeInsets.all(10.0),
-                      child: Container(
-                        height: 100,
-                        width: double.infinity,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                          color: Colors.white,
-                          boxShadow: [
-                            BoxShadow(
-                                color: Colors.black12,
-                                blurRadius: 20,
-                                spreadRadius: 10)
-                          ],
-                        ),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.all(10.0),
-                              child: CircleAvatar(
-                                backgroundColor: Colors.red,
-                                radius: 30,
-                                child: Text(
-                                  donorSnap['group'],
-                                  style: TextStyle(
+                itemCount: snapshot.data!.docs.length,
+                itemBuilder: (context, index) {
+                  final DocumentSnapshot donorSnap =
+                  snapshot.data.docs[index];
+                  return Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Container(
+                      height: 100,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Colors.white,
+                        boxShadow: [
+                          BoxShadow(
+                              color: Colors.black12,
+                              blurRadius: 20,
+                              spreadRadius: 10)
+                        ],
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(10.0),
+                            child: CircleAvatar(
+                              backgroundColor: Colors.red,
+                              radius: 30,
+                              child: Text(
+                                donorSnap['group'],
+                                style: TextStyle(
                                     color: Colors.white,
-                                      fontSize: 30,
-                                      fontWeight: FontWeight.w900),
-                                ),
+                                    fontSize: 30,
+                                    fontWeight: FontWeight.w900),
                               ),
                             ),
-                            Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Text(
-                                  donorSnap['name'],
-                                  style: TextStyle(
-                                      fontSize: 18, fontWeight: FontWeight.w700),
+                          ),
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                donorSnap['name'],
+                                style: TextStyle(
+                                    fontSize: 18, fontWeight: FontWeight.w700),
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              Text(
+                                donorSnap['phone'].toString(),
+                                style: TextStyle(
+                                    fontSize: 14, fontWeight: FontWeight.w700),
+                              ),
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              IconButton(
+                                onPressed: () {
+                                  Navigator.pushNamed(context, '/update',
+                                      arguments: {
+                                        'name': donorSnap['name'],
+                                        'phone': donorSnap['phone'].toString(),
+                                        'group': donorSnap['group'],
+                                        'id': donorSnap.id,
+                                      });
+                                },
+                                icon: Icon(
+                                  Icons.edit,
+                                  size: 20,
                                 ),
-                                SizedBox(height: 10,),
-                                Text(
-                                  donorSnap['phone'].toString(),
-                                  style: TextStyle(
-                                      fontSize: 14, fontWeight: FontWeight.w700),
-                                ),
-                              ],
-                            ),
-                            Row(
-                              children: [
-                                IconButton(
-                                  onPressed: () {
-                                    Navigator.pushNamed(context, '/update',
-                                        arguments: {
-                                          'name': donorSnap['name'],
-                                          'phone': donorSnap['phone'].toString(),
-                                          'group': donorSnap['group'],
-                                          'id': donorSnap.id,
-                                        });
-                                  },
-                                  icon: Icon(Icons.edit,size: 20,),
-                                  iconSize: 30,
-                                ),
-                                IconButton(
-                                  onPressed: () {
-                                    deleteDonor(donorSnap.id);
-                                  },
-                                  icon: Icon(Icons.delete),
-                                  iconSize: 20,
-                                  color: Colors.deepOrange,
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
+                                iconSize: 30,
+                              ),
+                              IconButton(
+                                onPressed: () {
+                                  deleteDonor(donorSnap.id);
+                                },
+                                icon: Icon(Icons.delete),
+                                iconSize: 20,
+                                color: Colors.deepOrange,
+                              ),
+                            ],
+                          ),
+                        ],
                       ),
-                    );
-                  });
+                    ),
+                  );
+                },
+              );
+            } else {
+              return Container(); // You can customize this for your use case
             }
-            return Container();
           },
         ),
       ),
