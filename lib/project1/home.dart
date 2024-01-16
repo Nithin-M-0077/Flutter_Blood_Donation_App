37390f32ba943be8407b285160add7b332efb873
Nithin-M-007 <isnithinm2001@gmail.com>
2024-01-16 20:53:10 +0530

    searchbar added

--- lib/project1/home.dart
+++ lib/project1/home.dart
@@ -12,8 +12,26 @@ class _HomePageState extends State<HomePage> {
   final CollectionReference donor =
   FirebaseFirestore.instance.collection('donor');
 
+  TextEditingController searchController = TextEditingController();
+  late Stream<QuerySnapshot> _stream;
+
   void deleteDonor(docId) {
     donor.doc(docId).delete();
+  }
+
+  void updateSearchResults(String query) {
+    setState(() {
+      _stream = donor
+          .where('name', isGreaterThanOrEqualTo: query)
+          .where('name', isLessThan: query + 'z')
+          .snapshots();
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _stream = donor.snapshots();
   }
 
   @override
@@ -41,114 +59,163 @@ class _HomePageState extends State<HomePage> {
         ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
-      body: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: StreamBuilder(
-          stream: donor.snapshots(),
-          builder: (context, AsyncSnapshot snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return Center(
-                child: CircularProgressIndicator(color: Colors.red,),
-              );
-            } else if (snapshot.hasError) {
-              return Center(
-                child: Text('Error: ${snapshot.error}'),
-              );
-            } else if (snapshot.hasData) {
-              return ListView.builder(
-                itemCount: snapshot.data!.docs.length,
-                itemBuilder: (context, index) {
-                  final DocumentSnapshot donorSnap =
-                  snapshot.data.docs[index];
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: TextField(
+              style: TextStyle(
+                fontSize: 13,
+                color: Colors.white,
+                fontWeight: FontWeight.w800,
+              ),
+              cursorColor: Colors.red,
+              controller: searchController,
+              decoration: InputDecoration(
+                hintText: 'Search...',
+                hintStyle: TextStyle(
+                  fontSize: 13,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w800,
+                ),
+                contentPadding: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                suffixIcon: IconButton(
+                  icon: Icon(
+                    Icons.clear,
+                    color: Colors.white,
+                  ),
+                  onPressed: () {
+                    searchController.clear();
+                    updateSearchResults('');
+                  },
+                ),
+                filled: true,
+                fillColor: Colors.red,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12),
+                  borderSide: BorderSide.none,
+                ),
+              ),
+              onChanged: (value) {
+                updateSearchResults(value);
+              },
+            ),
+          ),
+          Expanded(
+            child: StreamBuilder(
+              stream: _stream,
+              builder: (context, AsyncSnapshot snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Center(
+                    child: CircularProgressIndicator(color: Colors.red),
+                  );
+                } else if (snapshot.hasError) {
+                  return Center(
+                    child: Text('Error: ${snapshot.error}'),
+                  );
+                } else if (snapshot.hasData) {
                   return Padding(
                     padding: const EdgeInsets.all(10.0),
-                    child: Container(
-                      height: 100,
-                      width: double.infinity,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        color: Colors.white,
-                        boxShadow: [
-                          BoxShadow(
-                              color: Colors.black12,
-                              blurRadius: 20,
-                              spreadRadius: 10)
-                        ],
-                      ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.all(10.0),
-                            child: CircleAvatar(
-                              backgroundColor: Colors.red,
-                              radius: 30,
-                              child: Text(
-                                donorSnap['group'],
-                                style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 30,
-                                    fontWeight: FontWeight.w900),
-                              ),
+                    child: ListView.builder(
+                      itemCount: snapshot.data!.docs.length,
+                      itemBuilder: (context, index) {
+                        final DocumentSnapshot donorSnap =
+                        snapshot.data.docs[index];
+                        return Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: Container(
+                            height: 100,
+                            width: double.infinity,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              color: Colors.white,
+                              boxShadow: [
+                                BoxShadow(
+                                    color: Colors.black12,
+                                    blurRadius: 20,
+                                    spreadRadius: 10)
+                              ],
+                            ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.all(10.0),
+                                  child: CircleAvatar(
+                                    backgroundColor: Colors.red,
+                                    radius: 30,
+                                    child: Text(
+                                      donorSnap['group'],
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 30,
+                                          fontWeight: FontWeight.w900),
+                                    ),
+                                  ),
+                                ),
+                                Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Text(
+                                      donorSnap['name'],
+                                      style: TextStyle(
+                                          fontSize: 18,
+                                          fontWeight: FontWeight.w700),
+                                    ),
+                                    SizedBox(
+                                      height: 10,
+                                    ),
+                                    Text(
+                                      donorSnap['phone'].toString(),
+                                      style: TextStyle(
+                                          fontSize: 14,
+                                          fontWeight: FontWeight.w700),
+                                    ),
+                                  ],
+                                ),
+                                Row(
+                                  children: [
+                                    IconButton(
+                                      onPressed: () {
+                                        Navigator.pushNamed(context, '/update',
+                                            arguments: {
+                                              'name': donorSnap['name'],
+                                              'phone':
+                                              donorSnap['phone'].toString(),
+                                              'group': donorSnap['group'],
+                                              'id': donorSnap.id,
+                                            });
+                                      },
+                                      icon: Icon(
+                                        Icons.edit,
+                                        size: 20,
+                                      ),
+                                      iconSize: 30,
+                                    ),
+                                    IconButton(
+                                      onPressed: () {
+                                        deleteDonor(donorSnap.id);
+                                      },
+                                      icon: Icon(Icons.delete),
+                                      iconSize: 20,
+                                      color: Colors.deepOrange,
+                                    ),
+                                  ],
+                                ),
+                              ],
                             ),
                           ),
-                          Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text(
-                                donorSnap['name'],
-                                style: TextStyle(
-                                    fontSize: 18, fontWeight: FontWeight.w700),
-                              ),
-                              SizedBox(
-                                height: 10,
-                              ),
-                              Text(
-                                donorSnap['phone'].toString(),
-                                style: TextStyle(
-                                    fontSize: 14, fontWeight: FontWeight.w700),
-                              ),
-                            ],
-                          ),
-                          Row(
-                            children: [
-                              IconButton(
-                                onPressed: () {
-                                  Navigator.pushNamed(context, '/update',
-                                      arguments: {
-                                        'name': donorSnap['name'],
-                                        'phone': donorSnap['phone'].toString(),
-                                        'group': donorSnap['group'],
-                                        'id': donorSnap.id,
-                                      });
-                                },
-                                icon: Icon(
-                                  Icons.edit,
-                                  size: 20,
-                                ),
-                                iconSize: 30,
-                              ),
-                              IconButton(
-                                onPressed: () {
-                                  deleteDonor(donorSnap.id);
-                                },
-                                icon: Icon(Icons.delete),
-                                iconSize: 20,
-                                color: Colors.deepOrange,
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
+                        );
+                      },
                     ),
                   );
-                },
-              );
-            } else {
-              return Container(); // You can customize this for your use case
-            }
-          },
-        ),
+                } else {
+                  return Container();
+                }
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
